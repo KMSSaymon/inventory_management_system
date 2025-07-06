b3fc69f4a8f083e51377b9e5a45488054b593706
KM Shofiullah Saymon <shofiullahsaymon@gmail.com>
2025-07-06 21:22:33 +0600

    Fixed ALLOWED_HOSTS for Render

--- super_shop_project/settings.py
+++ super_shop_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v%*q=0o8qdux%v^40s5ei$3qvt287p=*peid^+72g9jsn9p13y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inventory-management-system-t80c.onrender.com']
 
 
 # Application definition
