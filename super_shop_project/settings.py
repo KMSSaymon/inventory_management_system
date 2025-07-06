971bc20cdebcf6a7d9009c8f1f01237a3faf8962
KM Shofiullah Saymon <shofiullahsaymon@gmail.com>
2025-07-06 21:15:45 +0600

    Switched from MySQL to SQLite for Render deployment

--- super_shop_project/settings.py
+++ super_shop_project/settings.py
@@ -81,14 +81,11 @@ WSGI_APPLICATION = 'super_shop_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'super_shop',
-        'USER': 'root',
-        'PASSWORD': '',  
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
